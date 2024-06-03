ef49a9bcf779932bd519023d08b59780c6db46dd
Maria Eduarda Ferreira Batista <ferreirabatistam34@gmail.com>
2024-06-03 18:50:32 -0300

    feat: adding the interface of Fornecedor

---
+++ src/interfaces/Fornecedor.ts
@@ -0,0 +1,15 @@
+export interface Fornecedor{
+    id: number;
+    empresa: string;
+    cnpj: string;
+    telefone: string;
+    email:string;
+    cep: string;
+    cidade: string;
+    bairro: string;
+    logradouro: string;
+    site: string;
+    area_de_atuacao: string;
+    produto_servico_oferecido: string;
+
+}
