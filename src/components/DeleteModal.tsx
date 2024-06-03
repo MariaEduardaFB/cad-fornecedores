5f4b6dd636613b3cf1ff75f45233d29db268d101
Maria Eduarda Ferreira Batista <ferreirabatistam34@gmail.com>
2024-06-03 19:15:38 -0300

    feat: creating delete modal

---
+++ src/components/DeleteModal.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ModalComponent from './ModalComp'
+import { Button } from 'antd'
+
+interface DeleteFornecedorModalProps{
+    visible: boolean 
+    onDelete: () => void
+    onCancel: () => void
+}
+
+const DeleteFornecedorModal: React.FC<DeleteFornecedorModalProps> =({visible, onDelete, onCancel}) => {
+    return(
+        <ModalComponent visible={visible} onCancel={onCancel}>
+            <h3>Tem certeza que deseja excluir esse fornecedor?</h3>
+            <Button type="primary" danger onClick={onDelete}>
+                Excluir
+            </Button>
+            <Button onClick={onCancel} style={{ marginLeft: '8px' }}>
+                Cancelar
+            </Button>
+    </ModalComponent>
+    )
+}
+
+export default DeleteFornecedorModal
