a4bc3e466a0bc84e71770639ac9b28d7640cd87b
Maria Eduarda Ferreira Batista <ferreirabatistam34@gmail.com>
2024-06-03 18:52:57 -0300

    feat: creating the modal for edit fornecedor

---
+++ src/components/EditModal.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import ModalComponent from './ModalComp'
+import FornecedorForm from "./FormFornecedor"
+import { Fornecedor } from '../interfaces/Fornecedor'
+
+interface EditFornecedorModalProps {
+    visible: boolean
+    fornecedor: Fornecedor
+    onSave: (fornecedor: Omit<Fornecedor, 'id'>) => void
+    onCancel: () => void
+}
+
+const EditFornecedorModal: React.FC<EditFornecedorModalProps> = ({ visible, fornecedor, onSave, onCancel }) => {
+    return(
+        <ModalComponent visible={visible} onCancel={onCancel}>
+            <h3>Editar Fornecedor</h3>
+            <FornecedorForm initialValues={fornecedor} onSave={onSave} onCancel={onCancel}></FornecedorForm>
+
+        </ModalComponent>
+    )
+}
