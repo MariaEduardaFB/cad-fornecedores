26bd40c526d2bb1e0834419b9de28c71777f4e31
Maria Eduarda Ferreira Batista <ferreirabatistam34@gmail.com>
2024-06-09 17:53:34 -0300

    fix: visible was incorrect

--- src/components/ModalComp.tsx
+++ src/components/ModalComp.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import { Modal } from 'antd'
+import React from 'react';
+import { Modal } from 'antd';
+import '../assets/styles/ModalComp.css';
 
 interface ModalComponentProps {
-    visible: boolean;
-    onCancel: () => void;
-    children: React.ReactNode
+  visible: boolean;
+  onCancel: () => void;
+  children: React.ReactNode;
 }
 
-const ModalComponent: React.FC<ModalComponentProps> = ({ visible, onCancel, children}) => {
-    return (
-        <Modal visible={visible} onCancel={onCancel} footer={null}>
-            {children}
-        </Modal>
+const ModalComponent: React.FC<ModalComponentProps> = ({ visible, onCancel, children }) => {
+  return (
+    <Modal
+      open={visible}
+      onCancel={onCancel}
+      footer={null}
+      className="custom-modal"  // Adicione a classe personalizada aqui
+    >
+      {children}
+    </Modal>
+  );
+};
 
-    )
-}
-
-export default ModalComponent
+export default ModalComponent;
